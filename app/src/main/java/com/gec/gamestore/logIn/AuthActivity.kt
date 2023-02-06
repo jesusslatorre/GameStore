dff394b7abc32777ee4bddc5e33f8e1a0a500765
Jesús Latorre Álvarez <102959649+jesusslatorre@users.noreply.github.com>
2023-02-06 11:41:14 +0100

    Update AuthActivity.kt
    Fixed Google Sign In auth.

--- app/src/main/java/com/gec/gamestore/logIn/AuthActivity.kt
+++ app/src/main/java/com/gec/gamestore/logIn/AuthActivity.kt
@@ -1,17 +1,22 @@
 package com.gec.gamestore.logIn
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.gec.gamestore.*
 import com.gec.gamestore.MainActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
@@ -19,7 +24,8 @@ import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
 
-    private val googleSignIn = 100
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,7 +48,72 @@ class AuthActivity : AppCompatActivity() {
         setup()
         session()
 
+        //Google
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient.signOut()
+
+        findViewById<Button>(R.id.googleButton).setOnClickListener {
+            signInGoogle()
+        }
     }
+
+    private fun signInGoogle() {
+
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+
+
+        }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+
+
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(baseContext, "¡Bienvenido/a!", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+    }
+
 
     override fun onStart() {
         super.onStart()
@@ -59,8 +130,6 @@ class AuthActivity : AppCompatActivity() {
             authLayout.visibility = View.INVISIBLE
 
         }
-
-
     }
 
     //Función de login y autenticación
@@ -103,18 +172,6 @@ class AuthActivity : AppCompatActivity() {
             startActivity(cuentaIntent)
         }
 
-        googleButton.setOnClickListener {
-
-            // Configuración autenticación con Google
-            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
-            val googleClient = GoogleSignIn.getClient(this, googleConf)
-            googleClient.signOut()
-
-            startActivityForResult(googleClient.signInIntent, googleSignIn)
-        }
     }
 
     private fun showAlert() {
@@ -135,47 +192,6 @@ class AuthActivity : AppCompatActivity() {
         val scrollIntent = Intent(this, MainActivity::class.java)
         startActivity(scrollIntent)
     }
-
-    //Autenticación con Google
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == googleSignIn) {
-
-            //Variable task para autenticar con Google
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-
-            //Bloque try, catch que obtiene el token de Google y registra en Firebase
-            try {
-                val account = task.getResult(ApiException::class.java)
-
-                if (account != null) {
-
-                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-
-                    FirebaseAuth.getInstance().signInWithCredential(credential)
-                        .addOnCompleteListener {
-
-                            if (it.isSuccessful) {
-
-                                Toast.makeText(baseContext, "¡Bienvenido/a!", Toast.LENGTH_SHORT)
-                                    .show()
-                                showScroll()
-                            } else {
-
-                                //Manejamos error, llamamos a diálogo
-                                showAlert()
-                            }
-                        }
-                }
-            } catch (e: ApiException) {
-
-                //Manejamos error, llamamos a diálogo
-                showAlert()
-            }
-        }
-    }
-
 }
 
 
